9480caf700961fc24560347084054ae5389a6f26
MariaEugeniaGR <gonzalezrangel.me@gmail.com>
2022-09-13 00:01:47 -0300

    Frontend

---
+++ src/app/components/hys1/hys1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hys1',
+  templateUrl: './hys1.component.html',
+  styleUrls: ['./hys1.component.css']
+})
+export class Hys1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
